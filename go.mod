3f5b117ddd453e9b4682a8635956b267f6ff7cc8
4livestudio <34774173+4livestudio@users.noreply.github.com>
2022-10-11 12:21:56 +0200

    Ver_Perfil_ok

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/rs/cors v1.8.2
 	go.mongodb.org/mongo-driver v1.10.3
 	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b
+	golang.org/x/tools v0.1.12 // indirect
 )
--- middlew/validoJWT.go
+++ middlew/validoJWT.go
@@ -2,17 +2,17 @@ package middlew
 
 import (
 	"net/http"
-	"github.com/4LiveStudioGit/routers"
+	"github.com/4LiveStudioGit/GO_twittor/routers"
 )
 
 // Funcion para validar el JWT que nos devuelve la petición
-func ValidoJWT(next htt.HandlerFunc) http.HandlerFunc{
+func ValidoJWT(next http.HandlerFunc) http.HandlerFunc{
 	return func(w http.ResponseWriter, r *http.Request){
-		_, _, _, err := routes.ProcesarToken(r.Header.Get("Authorization"))
+		_, _, _, err := routers.ProcesarToken(r.Header.Get("Authorization"))
 		if err != nil{
 			http.Error(w, "Error en el token!"+err.Error(), http.StatusBadRequest)
 			return
 		}
-		next.ServeHttp(w, r)
+		next.ServeHTTP(w, r)
 	}
 }
